eb64d2b9f56eda1b98f2dce71fec25dddd20bd78
clem <clemer19@gmail.com>
2014-04-03 16:21:33 -0500

    Add meeting notes from April 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Collaborateur::Application.routes.draw do
+
+  resources :profiles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'bulletin#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
